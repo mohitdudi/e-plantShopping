fecb8d38f94f1e1a06feed45db57b394f160f044
mohitdudi <mohit.di@proton.me>
2026-06-23 14:01:25 +0530

    Removed the default cart items

--- src/redux/CartSlice.jsx
+++ src/redux/CartSlice.jsx
@@ -5,24 +5,7 @@ const CartSlice = createSlice({
     name: 'cart',
     initialState: {
         size: 2,
-        items: [
-            {
-                    name: "Snake Plant",
-                    image: "https://cdn.pixabay.com/photo/2021/01/22/06/04/snake-plant-5939187_1280.jpg",
-                    description: "Produces oxygen at night, improving air quality.",
-                    cost: "$15",
-                    inCart: true,
-                    quantity: 1
-                },
-                {
-                    name: "Spider Plant",
-                    image: "https://cdn.pixabay.com/photo/2018/07/11/06/47/chlorophytum-3530413_1280.jpg",
-                    description: "Filters formaldehyde and xylene from the air.",
-                    cost: "$12",
-                    inCart: true,
-                    quantity: 1
-                },
-        ]
+        items: []      
     },
     reducers: {
         addToCart: (state, action) => {
